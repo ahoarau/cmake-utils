0e3b93586113e5e29dcd398d1f33bf4ee4215464
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2025-11-15 07:19:52 +0100

    read line by line instead of the whole file

--- modules/check_python_module_name.py
+++ modules/check_python_module_name.py
@@ -12,8 +12,10 @@ if not os.path.isfile(path) or os.path.splitext(path)[1] not in {".so", ".pyd"}:
 
 mod_name = None
 with open(path, "rb") as f:
-    if match := re.search(b"PyInit_([a-zA-Z0-9_]+)", f.read()):
-        mod_name = match.group(1).decode("utf-8")
+    for line in f:
+        if match := re.search(b"PyInit_([a-zA-Z0-9_]+)", line):
+            mod_name = match.group(1).decode("utf-8")
+            break
 
 if len(sys.argv) == 3:
     if mod_name != sys.argv[2]:
